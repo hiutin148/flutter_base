6248e68dab09d51987a9815b85cb9971fd4f3eac
hieunt <hieunt@magenest.com>
2024-12-06 11:42:25 +0700

    init

---
+++ lib/main_dev.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'app.dart';
+import 'configs/app_configs.dart';
+import 'configs/app_env_config.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  AppConfigs.env = Environment.dev;
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
